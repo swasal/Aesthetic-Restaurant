9fea61f00b7529c7ef0227b2c471c0e8e2a8af4d
swasal <144431311+swasal@users.noreply.github.com>
2024-12-05 22:05:28 +0600

    feat: add base layout and menu page with backend setup
    This commit adds the base.html for the main layout and menu.html for the menu page, along with the backend setup in app.py to support these pages. The backend routes are now in place to render the menu page dynamically.

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("base.html", title="Home")
+
+
+
+@app.route('/menu')
+def menu():
+    # Get champion data based on champion_id
+    menu = [
+        ['item_id', 'Cake', 'A vanilla cake', "A description of the item"],
+        ['item_id', 'Cake', 'A vanilla cake', "A description of the item"],
+        ['item_id', 'Cake', 'A vanilla cake', "A description of the item"],
+    ]
+    
+    return render_template('menu.html', title="Menu", menu=menu)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

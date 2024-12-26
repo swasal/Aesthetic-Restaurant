e6644b6505bcf2bb9dc9e034f170fe994f56e1ac
swasal <144431311+swasal@users.noreply.github.com>
2024-12-26 17:40:09 +0600

    feature/ admin-menu and dietary-recommendations
    -added dietary recommendation tot he menu page
    -added a menu description for detailed view of the menu

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import Flask, render_template, request, redirect, url_for
 #global
 
 users = [] # In-memory storage for user data
+userlogin=True #track if user is logged in
 
 
 
@@ -22,13 +23,18 @@ app = Flask(__name__)
 
 
 
+#testing routes
+
+
+
 
 #routing traffic
-
 
 @app.route("/")
 def home():
     return render_template("index.html", title="Home")
+
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -50,7 +56,9 @@ def login():
         return redirect(url_for('login'))
 
     # Render the login form
-    return render_template('login.html')
+    return render_template('login.html', title="Login")
+
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -85,7 +93,7 @@ def register():
         return redirect(url_for('login'))
 
     # Render the register form
-    return render_template('register.html')  # Display the sign-in form
+    return render_template('register.html', title="Register")  # Display the sign-in form
 
 
 
@@ -95,10 +103,11 @@ def profile():
 
 
 
-
 @app.route("/contact")
 def contact():
     return render_template("contact.html", title="Contact Us")
+
+
 
 @app.route('/ordersummary')
 def ordersummary():
@@ -109,19 +118,49 @@ def ordersummary():
     ]
     return render_template('order_summary.html', title="Order Summary", ordersummary=ordersummary)
 
+
+
 @app.route("/reservation")
 def reservation():
     return render_template("reservation.html", title="Reservation")
 
-@app.route('/menu')
-def menu():
-    # Sample menu data
-    menu = [
-        ['item_id', 'Cake', 'A vanilla cake', "A description of the item"],
-        ['item_id', 'Cake', 'A vanilla cake', "A description of the item"],
-        ['item_id', 'Cake', 'A vanilla cake', "A description of the item"],
-    ]
-    return render_template('menu.html', title="Menu", menu=menu)
 
+@app.route('/menu', defaults={'item_id': None})
+@app.route('/menu/<item_id>')
+def menu(item_id):
+    global userlogin
+
+    if item_id==None: #render menu page
+
+        # Sample menu data
+        menu = [
+            ['item_id_1', 'Cake', 'A vanilla cake', "A description of the item"],
+            ['item_id_2', 'Cake', 'A vanilla cake', "A description of the item"],
+            ['item_id_3', 'Cake', 'A vanilla cake', "A description of the item"],
+        ]
+
+        return render_template('menu.html', title="Menu", menu=menu, userlogin=userlogin)
+    
+
+    else: #render item description page
+
+        
+        class dish:
+            # Constructor to initialize the object with name and age
+            def __init__(self, name, item_id, description, picture, ingriedients, price):
+                self.name = name
+                self.id = item_id
+                self.description=description
+                self.picture=picture
+                self.ingriedients=ingriedients
+                self.price=price
+
+        item=dish("meaw", 1234, "Descrition of athe item from the db", "A list of the pictures", ["spices", "mayo"], 12.50)
+
+        return render_template('menu-description.html', title=f"{item_id}", item=item)
+
+
+
+#executing file
 if __name__ == "__main__":
     app.run(debug=True)

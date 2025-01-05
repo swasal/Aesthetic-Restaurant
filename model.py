32a03067b7bcd3007362b5bcc53a70af5d1ea4fc
JahanFab <114019535+JahanFab@users.noreply.github.com>
2025-01-05 19:05:17 +0600

    Updated model.py
    created admin, review, customer, employee, order, reservation

--- model.py
+++ model.py
@@ -27,10 +27,79 @@ class Employee():
 
 class Menu:
     def __init__(self, itemcode, name, description, ingredients, price):
-        # run an sql command to fetch the data
+        # run an sqlthe data command to fetch 
         self.itemcode=itemcode
         self.name=name
         self.description=description
         self.ingredients=ingredients
         self.price=price
 
+class Admin:
+    def __init__(self, admin_id, name, phone, email, password):
+    
+        self.admin_id = admin_id
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.password = password
+
+
+class Review:
+    def __init__(self, review_id, customer_id, date, content):
+        
+        self.review_id = review_id
+        self.customer_id = customer_id
+        self.date = date
+        self.content = content
+
+class Customer:
+    def __init__(self, customer_id, name, birthdate, phone, email, allergens, height, weight, address, preferred_ingredients, level_of_masala):
+        
+        self.customer_id = customer_id
+        self.name = name
+        self.birthdate = birthdate
+        self.phone = phone
+        self.email = email
+        self.allergens = allergens
+        self.height = height
+        self.weight = weight
+        self.address = address
+        self.preferred_ingredients = preferred_ingredients
+        self.level_of_masala = level_of_masala
+
+
+class Employee:
+    def __init__(self, employee_id, name, birthdate, phone, email, job_title, department, hired, address, slots, shift_date):
+        
+        self.employee_id = employee_id
+        self.name = name
+        self.birthdate = birthdate
+        self.phone = phone
+        self.email = email
+        self.job_title = job_title
+        self.department = department
+        self.hired = hired
+        self.address = address
+        self.slots = slots
+        self.shift_date = shift_date
+
+
+class Order:
+    def __init__(self, order_id, customer_id, order_datetime, total_amount, employee_id, status):
+    
+        self.order_id = order_id
+        self.customer_id = customer_id
+        self.order_datetime = order_datetime
+        self.total_amount = total_amount
+        self.employee_id = employee_id
+        self.status = status
+
+
+class Reservation:
+    def __init__(self, reservation_id, customer_id, date, time, number_of_guests):
+        
+        self.reservation_id = reservation_id
+        self.customer_id = customer_id
+        self.date = date
+        self.time = time
+        self.number_of_guests = number_of_guests
